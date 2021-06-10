612321198ea4fcdfa1b0ecfe0d4b54b8f3d0d3a7
unknown <muflihunnaim275@gmail.com>
2021-06-10 21:55:30 +0800

    init

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '7f147371d293faae7c59388c4d5591f5',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
